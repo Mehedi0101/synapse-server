4f71d428ea508f61775fd07770b0d83569b597dc
Mehedi0101 <mehedih2909@gmail.com>
2025-09-22 19:14:25 +0600

    method for updating and removing resources from DB is added

--- index.js
+++ index.js
@@ -1109,11 +1109,141 @@ async function run() {
         // ---------- resources ----------
         // -------------------------------
 
+        // all resources contributed by a user
+        app.get('/resources/my/:userId', async (req, res) => {
+            const { userId } = req.params;
+
+            const resources = await resourceCollection.aggregate([
+                { $match: { authorId: new ObjectId(userId) } },
+                {
+                    $lookup: {
+                        from: "users",
+                        localField: "authorId",
+                        foreignField: "_id",
+                        as: "authorDetails"
+                    }
+                },
+                { $unwind: { path: "$authorDetails", preserveNullAndEmptyArrays: true } },
+                {
+                    $addFields: {
+                        author: {
+                            _id: "$authorDetails._id",
+                            name: "$authorDetails.name",
+                            userImage: "$authorDetails.userImage",
+                            role: "$authorDetails.role",
+                            department: "$authorDetails.department"
+                        }
+                    }
+                },
+                { $project: { authorDetails: 0 } },
+                { $sort: { createdAt: -1 } }
+            ]).toArray();
+
+            res.send(resources);
+        });
+
+
+        // all resources except author's resources
+        app.get('/resources/all/:userId', async (req, res) => {
+            const { userId } = req.params;
+
+            const resources = await resourceCollection.aggregate([
+                { $match: { authorId: { $ne: new ObjectId(userId) } } },
+                {
+                    $lookup: {
+                        from: "users",
+                        localField: "authorId",
+                        foreignField: "_id",
+                        as: "authorDetails"
+                    }
+                },
+                { $unwind: { path: "$authorDetails", preserveNullAndEmptyArrays: true } },
+                {
+                    $addFields: {
+                        author: {
+                            _id: "$authorDetails._id",
+                            name: "$authorDetails.name",
+                            userImage: "$authorDetails.userImage",
+                            role: "$authorDetails.role",
+                            department: "$authorDetails.department"
+                        }
+                    }
+                },
+                { $project: { authorDetails: 0 } },
+                { $sort: { createdAt: -1 } }
+            ]).toArray();
+
+            res.send(resources);
+        });
+
+
+        // for getting details of a resource
+        app.get('/resources/details/:resourceId', async (req, res) => {
+            const { resourceId } = req.params;
+
+            const resource = await resourceCollection.aggregate([
+                { $match: { _id: new ObjectId(resourceId) } },
+                {
+                    $lookup: {
+                        from: "users",
+                        localField: "authorId",
+                        foreignField: "_id",
+                        as: "authorDetails"
+                    }
+                },
+                { $unwind: { path: "$authorDetails", preserveNullAndEmptyArrays: true } },
+                {
+                    $addFields: {
+                        author: {
+                            _id: "$authorDetails._id",
+                            name: "$authorDetails.name",
+                            userImage: "$authorDetails.userImage",
+                            role: "$authorDetails.role",
+                            department: "$authorDetails.department"
+                        }
+                    }
+                },
+                { $project: { authorDetails: 0 } },
+                { $sort: { createdAt: -1 } }
+            ]).toArray();
+
+            res.send(resource[0] || null);
+        });
+
+
         // for inserting a resource
         app.post('/resources', async (req, res) => {
             const resource = req.body;
             resource.createdAt = new Date();
+            resource.authorId = new ObjectId(resource.authorId);
             const result = await resourceCollection.insertOne(resource);
+            res.send(result);
+        })
+
+
+        // for updating a resource
+        app.patch('/resources/:resourceId', async (req, res) => {
+            const { resourceId } = req.params;
+            const updatedResource = req.body;
+
+            updatedResource._id = new ObjectId(updatedResource._id);
+            updatedResource.date = new Date(updatedResource.date);
+            updatedResource.authorId = new ObjectId(updatedResource.authorId);
+            
+            const result = await resourceCollection.updateOne(
+                { _id: new ObjectId(resourceId) },
+                {
+                    $set: updatedResource
+                }
+            );
+            res.send(result);
+        })
+
+
+        // for removing a resource
+        app.delete('/resources/:resourceId', async (req, res) => {
+            const { resourceId } = req.params;
+            const result = await resourceCollection.deleteOne({ _id: new ObjectId(resourceId) });
             res.send(result);
         })
 
